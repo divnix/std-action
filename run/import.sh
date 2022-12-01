4a7260ad67447364b5d84552ceb62c94a57c908e
Timothy DeHerrera <tim@nrdxp.dev>
2022-11-30 20:26:19 -0700

    run: fix import

--- run/import.sh
+++ run/import.sh
@@ -5,6 +5,10 @@ set -e
 function import() {
   echo "::group::Import Discovery"
 
+  mkdir -p "$DISC_PATH"
+
+  tar -C "$DISC_PATH" --zstd -f "$DISC_ARC_PATH" -x .
+
   if [[ $NIX_KEY_PATH != "" ]]; then
     nix store sign -r -k "$NIX_KEY_PATH" --all --store "$DISC_PATH"
   fi
