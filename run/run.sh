9d771886f898b28a392789f098dcdb265900ca1d
Timothy DeHerrera <tim@nrdxp.dev>
2022-12-14 16:58:01 -0700

    run: fix action script not existing

--- run/run.sh
+++ run/run.sh
@@ -10,21 +10,26 @@ function check_exec() {
 }
 
 function run() {
-  local action drv
+  local action drv name target
 
-  jq -r '.action + " " + .name + " " + .actionDrv' <<< "$JSON" | read -r action name drv
+  jq -r '.action + " " + .name + " " + .actionDrv + " " + .targetDrv' <<< "$JSON" | read -r action name drv target
 
   echo "::group::$action $name"
 
   if [[ -z $BUILT ]]; then
     # should be fetched, since we have already checked cache status in build step
-    nix-build "$drv" --no-out-link
+    nix-build "$target" "$drv" --no-out-link
   elif [[ $BUILDER != auto ]]; then
-    nix copy --from "$BUILDER" "$drv"
+    nix copy --from "$BUILDER" "$target"
   fi
 
   out="$(nix show-derivation "$drv" | jq -r '.[].outputs.out.path')"
-  "$out"
+  if [[ -a $out ]]; then
+    "$out"
+  else
+    nix-build "$drv" --no-out-link
+    "$out"
+  fi
 
   echo "::endgroup::"
 }
