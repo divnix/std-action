758eebdc23947ca0d0d334fb29be6c402f98a2ef
David Arnold <david.arnold@iohk.io>
2023-07-31 16:58:45 -0500

    fix: shellcheck and build action special case

--- run/execute.sh
+++ run/execute.sh
@@ -4,10 +4,8 @@ set -e
 set -o pipefail
 
 #shellcheck disable=SC2154
-
 if [[ "$DRV_IMPORT_FROM_DISCOVERY" == "false" ]]; then
-   cat "$EVALSTORE_IMPORT/$(basename $actionDrv).zst" \
-   | unzstd | nix-store --import &>/dev/null
+   unzstd < "$EVALSTORE_IMPORT/$(basename "$actionDrv").zst" | nix-store --import &>/dev/null
 else
    ssh discovery -- "nix-store --query --requisites $actionDrv | nix-store --stdin --export | zstd" \
    | unzstd | nix-store --import &>/dev/null
@@ -33,7 +31,7 @@ fi
 
 # Note about: sed $'s/\r\033\[0m\033\[K//g'
 #   works around https://discourse.nixos.org/t/nix-with-faketty/31042
-start=`date +%s`
+start=$(date +%s)
 faketty nix build "${build_args[@]}" "$actionDrv^out" \
   1> /dev/null \
   2> >(sed --unbuffered $'
@@ -48,36 +46,40 @@ faketty nix build "${build_args[@]}" "$actionDrv^out" \
     }
   ' >&2)
 
-echo "Copied $(cat ./copylogs | sed '/copying 0 .*/d' | wc -l) paths during the build."
-end=`date +%s`
+echo "Copied $(sed '/copying 0 .*/d' < ./copylogs | wc -l) paths during the build."
+end=$(date +%s)
 echo -e "\033[0;32mBuilt action closure in $((end-start)) seconds\033[0m"
 
 echo "::endgroup::"
 
 shopt -s lastpipe
 
-out="$(nix derivation show $actionDrv^out | jq -r '.[].outputs.out.path')"
-if [[ "$REMOTE_STORE" != "false" ]] && [[ ! -e "$out" ]]; then
-  echo "::group::🐟 fetch $action closure (from $REMOTE_STORE)"
-  start=`date +%s`
-  nix copy --from "$REMOTE_STORE" "$out" 1> /dev/null 2> >(sed --unbuffered "
-
-    # delete some lines that dont make sense
-    /^$/d
-    /don't know how to.*/d
-    \%^\s\s/nix/store/.*%d
-
-    # be less noisy - extract store path only
-    s/copying path '//g;s/'.*//g;
-
-  " >&2)
-  end=`date +%s`
-  echo -e "\033[0;32mFetched action closure in $((end-start)) seconds.\033[0m"
-  echo "::endgroup::"
-fi
 
 #shellcheck disable=SC2154
 if [[ "$action" != "build" ]]; then
+
+  out="$(nix derivation show "$actionDrv^out" | jq -r '.[].outputs.out.path')"
+
+  if [[ "$REMOTE_STORE" != "false" ]] && [[ ! -e "$out" ]]; then
+    echo "::group::🐟 fetch $action closure (from $REMOTE_STORE)"
+    {
+      start=$(date +%s)
+      nix copy --from "$REMOTE_STORE" "$out" 1> /dev/null 2> >(sed --unbuffered "
+  
+        # delete some lines that dont make sense
+        /^$/d
+        /don't know how to.*/d
+        \%^\s\s/nix/store/.*%d
+  
+        # be less noisy - extract store path only
+        s/copying path '//g;s/'.*//g;
+  
+      " >&2)
+      end=$(date +%s)
+      echo -e "\033[0;32mFetched action closure in $((end-start)) seconds.\033[0m"
+    }
+    echo "::endgroup::"
+  fi
 
   #shellcheck disable=SC2154
   echo "::group::🏍️️ $action //$cell/$block/$target"
@@ -86,12 +88,12 @@ if [[ "$action" != "build" ]]; then
     echo -e "\033[1;32m//$cell/$block/$target:$action\033[0m"
     echo -e "\033[1;32m.  $out\033[0m"
 
-    start=`date +%s`
+    start=$(date +%s)
     # this trick preserves set -e & set -o pipefail from above
     #shellcheck disable=SC1090
     function _run() { . "$out"; }
     _run
-    end=`date +%s`
+    end=$(date +%s)
     echo -e "\033[0;32mRan action in $((end-start)) seconds.\033[0m"
   }
   echo "::endgroup::"
